9011377081c5e5f38d55a7c1c97c805c9798fcc3
monkenWu <610877102@mail.nknu.edu.tw>
2020-06-28 23:58:45 +0800

    add init command

--- src/Commands/initLibrary.php
+++ src/Commands/initLibrary.php
@@ -1,21 +1,39 @@
-<?php 
-namespace SDPMlab\Commands;
+<?php namespace SDPMlab\Ci4Roadrunner\Commands;
+
 use CodeIgniter\CLI\BaseCommand;
 use CodeIgniter\CLI\CLI;
-use monken\Commands\CliCreate;
 
-class initLibrary extends BaseCommand{
-
+class InitLibrary extends BaseCommand
+{
     protected $group       = 'ciroad';
     protected $name        = 'ciroad:init';
     protected $description = 'Init all file.';
-    protected $usage = 'ciroad:init';
-    protected $arguments = [];
-    protected $options = [];
-        
-    public function run(array $params = []){
 
-        return;
+    public function run(array $params)
+    {
+        CLI::write(
+            CLI::color("Initializing Roadrunner Server binary ......\n","blue")
+        );
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $word = "&&";
+        }else{
+            $word = ";";
+        }
+        $init = popen("cd ".ROOTPATH."{$word}./vendor/bin/rr get", 'r');
+        pclose($init);
+        CLI::write(
+            CLI::color("\nCopy Codeigniter4 Roadrunner file ......\n","blue")
+        );
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            $word = "\\";
+        }else{
+            $word = "/";
+        }
+        copy(dirname(__FILE__).$word."file".$word."psr-worker",ROOTPATH."psr-worker.php");
+        copy(dirname(__FILE__).$word."file".$word.".rr.yaml",ROOTPATH.".rr.yaml");
+        CLI::write(
+            CLI::color("Initialization successful!\n", 'green')
+        );
     }
-
 }
+?>
